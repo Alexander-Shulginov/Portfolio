62b6cd8efad52d991987654547528bef82e5d95e
Alexander_721 <alexander3post@gmail.com>
2024-10-27 15:55:51 +0300

    add about contacts sections

--- src/js/observer.js
+++ src/js/observer.js
@@ -1,13 +1,12 @@
 const animated = () => {
 	const elementsToObserve = {
-		".works__example": "works__example--active",
-		".skills__item": "skills__item--active",
-		".skills__techno": "skills__techno--active",
-		".about__content": "about__content--active",
-		".hero__descr": "hero__descr--active",
-		".hero__photo": "hero__photo--active",
-		".contacts__item": "contacts__item--active",
-		"[class*=title]": "isVisible",
+		'.works__example': 'works__example--active',
+		'.skills__item': 'skills__item--active',
+		'.skills__techno': 'skills__techno--active',
+		'.about__content': 'about__content--active',
+		'.hero__descr': 'hero__descr--active',
+		'.hero__photo': 'hero__photo--active',
+		'[class*=title]': 'isVisible',
 	};
 
 	function onEntry(entry, classToAdd) {
@@ -19,23 +18,23 @@ const animated = () => {
 	}
 
 	function observeElements(selector, classToAdd) {
-		let options = {
+		const options = {
 			threshold: [0.4],
 		};
 
-		let observer = new IntersectionObserver((entry) => {
+		const observer = new IntersectionObserver((entry) => {
 			onEntry(entry, classToAdd);
 		}, options);
 
-		let elements = document.querySelectorAll(selector);
+		const elements = document.querySelectorAll(selector);
 
-		for (let elm of elements) {
+		for (const elm of elements) {
 			observer.observe(elm);
 		}
 	}
 
-	for (let selector in elementsToObserve) {
-		let classToAdd = elementsToObserve[selector];
+	for (const selector in elementsToObserve) {
+		const classToAdd = elementsToObserve[selector];
 		observeElements(selector, classToAdd);
 	}
 };
