09929e504579aede83fd17a889340b1281e72e87
Alexander_721 <alexander3post@gmail.com>
2024-11-19 21:56:59 +0300

    start update skills section

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
+import { gsap } from 'gsap';
+
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import burgeMenuInit from './burger';
 
 burgeMenuInit();
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to('.ss', {
+	scrollTrigger: {
+		trigger: '.skills',
+		start: '50% 100%',
+		scrub: true,
+		markers: true,
+	},
+
+	rotate: 40,
+});
